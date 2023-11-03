b8056d0f689d9eeb42896770ff4ad7bea9ea0e1f
Noway <ahmadrezamohammadi777@gmail.com>
2023-11-03 13:44:09 +0330

    First file (Not Completed)

---
+++ js/myApp.js
@@ -0,0 +1,40 @@
+const searchInput = document.getElementById('search-input');
+const products = document.querySelectorAll('.product-item');
+const buttons = document.querySelectorAll('.filter')
+
+
+const searchHandler = (event) => {
+    const searchValue = event.target.value.toLowerCase().trim();
+    products.forEach((product) => {
+        const productName = product.children[1].innerText.toLowerCase();
+        if (productName.includes(searchValue)) {
+            product.style.display = 'block'
+        } else {
+            product.style.display = 'none'
+        }
+    })
+}
+
+const filterHandler = (event) => {
+    const filter = event.target.dataset.filter;
+    products.forEach((product) => {
+        const category = product.dataset.category;
+        if (filter === 'all') {
+            product.style.display = 'block'
+        } else {
+            if (filter === category) {
+                product.style.display = 'block'
+            } else {
+                product.style.display = 'none'
+            }
+        }
+    })
+
+}
+
+searchInput.addEventListener('keyup', searchHandler);
+buttons.forEach((button) => {
+    button.addEventListener('click', filterHandler);
+})
+
+console.log('hi');
